1ab2e65cb64187d5a85f64bcba0be55e3b53099b
greysi <60203884+greysigriz@users.noreply.github.com>
2025-11-20 22:38:54 -0500

    correciones

--- backend/test_clasificador.py
+++
@@ -1,5 +0,0 @@
-from services.clasificador import ClasificadorDependencias
-
-clasificador = ClasificadorDependencias("dependencias_cancun.json")
-resultado = clasificador.clasificar("hay un bache en mi calle")
-print(resultado)
